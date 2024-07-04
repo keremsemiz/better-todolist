5ea03dde0a089f02d2f4527fc37659a10e09e103
Kerem Semiz <kerox350@gmail.com>
2024-07-04 19:44:22 +0300

    Initial Project Setup.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="box">
+      <img className="rectangle" alt="Rectangle" src="rectangle-15.png" />
+    </div>
+  );
+};
+export default Box
