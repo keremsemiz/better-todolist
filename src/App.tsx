9ec71c8d826e4e265301eeecb089975d5e969dc0
Kerem Semiz <kerox350@gmail.com>
2024-07-06 15:19:10 +0300

    Start work on the new and improved todo list (planned for arcade projs.)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from 'react';
 import './App.css';
 
-export const Box = (): JSX.Element => {
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([
+        ...todos,
+        { id: Date.now(), text: newTodo, completed: false }
+      ]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setNewTodo(event.target.value);
+  };
+
   return (
-    <div className="box">
-      <img className="rectangle" alt="Rectangle" src="rectangle-15.png" />
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        placeholder="Enter new task"
+      />
+      <button onClick={addTodo}>Add Task</button>
+      <ul>
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            onClick={() => toggleTodo(todo.id)}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
-export default Box
+}
+
+export default App;
